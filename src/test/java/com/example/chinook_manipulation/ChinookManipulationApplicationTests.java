0440f24dd4f4a5e1dd54e541491b891c50540b24
Christos Giannikis <xrist.giann@gmail.com>
2023-01-21 18:54:29 +0200

    SetUp, test commit

---
+++ src/test/java/com/example/chinook_manipulation/ChinookManipulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chinook_manipulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinookManipulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
