81768b46c6d519efda8234ab7f723de5dcd5bdac
Christos Giannikis <xrist.giann@gmail.com>
2023-01-23 17:30:54 +0200

    Edit the page() function to return a page of  customers from the database with limit and offset.

--- src/main/java/repositories/CustomerImpl.java
+++ src/main/java/repositories/CustomerImpl.java
@@ -41,25 +41,25 @@ public class CustomerImpl implements CustomerRepository{
 
     @Override
     public Customer readByID(Integer integer) {
-        String sql = "SELECT * FROM customer WHERE customer_id = ?";
-        Customer customer = null;
-        try(Connection conn = DriverManager.getConnection(url,username,password)){
-            PreparedStatement statement = conn.prepareStatement(sql);
-            statement.setInt(1,integer);
-            ResultSet result = statement.executeQuery();
-            customer = new Customer(
-                    result.getInt("customer_id"),
-                    result.getString("first_name"),
-                    result.getString("last_name"),
-                    result.getString("country"),
-                    result.getInt("postal_code"),
-                    result.getInt("phone"),
-                    result.getString("email")
+        String sql = "SELECT * FROM customer WHERE customer_id = ?"; //passing the sql query into a string value
+        Customer customer = null; //creating a new customer reference
+        try(Connection conn = DriverManager.getConnection(url,username,password)){ //trying to make a connection with the database
+            PreparedStatement statement = conn.prepareStatement(sql); //creating a statement for the query
+            statement.setInt(1,integer); //replacing the question mark with the integer
+            ResultSet result = statement.executeQuery(); //making a result set and executing the query
+            customer = new Customer( //making a new customer
+                    result.getInt("customer_id"),   //adding the customers' id from databases "customer_id" field
+                    result.getString("first_name"), //adding the customers' name from databases "first_name" field
+                    result.getString("last_name"),  //adding the customers' surname from databases "last_name" field
+                    result.getString("country"),    //adding the customers' country from databases "country" field
+                    result.getInt("postal_code"),   //adding the customers' postal code from databases "postal_code" field
+                    result.getInt("phone"),         //adding the customers' phone from databases "phone" field
+                    result.getString("email")      //adding the customers' email from databases "email" field
             );
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println(e.getMessage()); //prints the exceptions' message
         }
-        return customer;
+        return customer; //returns the customer
     }
 
     @Override
@@ -69,8 +69,29 @@ public class CustomerImpl implements CustomerRepository{
 
     @Override
     public List<Customer> page(int limit, int offset) {
-
-        return null;
+        String sql = "SELECT first_name, last_name, phone, email FROM customer LIMIT ? OFFSET ?"; //passing the sql query into a string value
+        List<Customer> customers = null; //creating a new list of customers
+        Customer customer = null; //creating a new customer reference
+        try(Connection conn = DriverManager.getConnection(url,username,password)){ //trying to make a connection with the database
+            PreparedStatement statement = conn.prepareStatement(sql); //creating a statement for the query
+            statement.setInt(1,limit);  //replacing the first question mark with the limit
+            statement.setInt(2,offset); //replacing the first question mark with the offset
+            ResultSet result = statement.executeQuery(); //making a result set and executing the query
+            while(result.next()){ //for every customer in the database that returns the query
+                customer = new Customer( //making a new customer
+                        result.getInt("customer_id"), //adding the customers' id from databases "customer_id" field
+                        result.getString("first_name"), //adding the customers' name from databases "first_name" field
+                        result.getString("last_name"), //adding the customers' surname from databases "last_name" field
+                        result.getString("country"), //adding the customers' country from databases "country" field
+                        result.getInt("postal_code"), //adding the customers' postal code from databases "postal_code" field
+                        result.getInt("phone"), //adding the customers' phone from databases "phone" field
+                        result.getString("email"));//adding the customers' email from databases "email" field
+                customers.add(customer);
+            }
+        }catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return customers;
     }
 
         @Override
